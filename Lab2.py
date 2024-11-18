3d901abc62444c711f03bf6c5656e49778abaccd
FelixChan <felixchan.22@ichat.sp.edu.sg>
2024-11-18 21:51:10 +0800

    testttt

---
+++ Lab2.py
@@ -0,0 +1,34 @@
+def main():
+    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+    display_main_menu()
+    num_list = get_user_input()
+
+    # Function to display the main menu
+def display_main_menu():
+    print("display_main_menu")
+
+# Function to get user input 
+def get_user_input():
+    print("get_user_input")
+    return []
+
+# Function to calculate the average of a list 
+def calc_average(temp_list):
+    print("calc_average")
+    return 0.0
+
+# Function to find the minimum and maximum of a list 
+def find_min_max(temp_list):
+    print("find_min_max")
+    return [0.0, 0.0]
+
+# Function to sort the temperatures in ascending order 
+def sort_temperature(temp_list):
+    print("sort_temperature")
+    return []
+
+# Function to calculate the median temperature 
+def calc_median_temperature(temp_list):
+    print("calc_median_temperature")
+    return 0.0
+
